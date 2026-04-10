e65921931ba84c25a0cba424f736e305c7429839
HitryDmitry <ksenon.104@gmail.com>
2026-04-10 13:21:48 +0300

    Showing icons in QListWidget, start implementing drag&drop

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include "ComponentLibraryManager.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ComponentLibraryManager library;
+    library.loadFromJson(":/data/components.json");
+
+    for (const auto &comp : library.getComponents()) {
+        QListWidgetItem *item = new QListWidgetItem(QIcon(comp.iconPath), comp.name);
+
+        item->setData(Qt::UserRole, comp.type);
+        ui->listWidget->addItem(item);
+    }
 }
 
 MainWindow::~MainWindow()
