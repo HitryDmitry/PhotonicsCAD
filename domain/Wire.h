4521ccbcc1d7eb7d7ae1b8e4dda8faf64981b1c4
HitryDmitry <ksenon.104@gmail.com>
2026-05-21 14:09:39 +0300

    add constructor, == overload and qHash to Wire.h for storing in QSet

--- domain/Wire.h
+++ domain/Wire.h
@@ -1,8 +1,28 @@
+#pragma once
+#include <QHash>
 #include "PinInstance.h"
 
 class Wire
 {
 public:
-    PinInstance *from = nullptr;
-    PinInstance *to = nullptr;
+    Wire(PinInstance *startPin = nullptr, PinInstance *endPin = nullptr)
+        : from(startPin)
+        , to(endPin)
+    {}
+
+    bool operator==(const Wire &other) const
+    {
+        return (from == other.from && to == other.to) || (from == other.to && to == other.from);
+    }
+
+    friend size_t qHash(const Wire &wire, size_t seed = 0)
+    {
+        // Комбинирование хэшей с перемешиванием для лучшего распределения
+        size_t h1 = qHash(wire.from, seed);
+        size_t h2 = qHash(wire.to, seed);
+        return h1 ^ h2;
+    }
+
+    PinInstance *from;
+    PinInstance *to;
 };
