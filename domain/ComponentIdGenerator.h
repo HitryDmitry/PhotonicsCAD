b12b94aae7dd751e863426aaedeacfb2afa55ece
HitryDmitry <ksenon.104@gmail.com>
2026-08-17 13:37:17 +0300

    Возможность сбросить счетчик компонентов (при создании новой схемы, например)

--- domain/ComponentIdGenerator.h
+++ domain/ComponentIdGenerator.h
@@ -5,6 +5,8 @@ class ComponentIdGenerator
 public:
     ComponentId generateNext() { return ComponentId(++m_nextId); }
 
+    void reset() { m_nextId = 0; }
+
 private:
     std::uint64_t m_nextId{0};
 };
