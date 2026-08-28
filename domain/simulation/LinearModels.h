486eb9aa51d08ec999df0beb3f158e376d52bcf3
HitryDmitry <ksenon.104@gmail.com>
2026-08-28 16:46:00 +0300

    Рефакторинг комментариев LinearModels

--- domain/simulation/LinearModels.h
+++ domain/simulation/LinearModels.h
@@ -1,18 +1,12 @@
 #pragma once
 #include "IComponentModel.h"
 
-// Модель источника (Лазер)
 class LaserModel : public IComponentModel {
 public:
-    std::complex<double> transferFunction(double frequency) override {
-        return {1.0, 0.0}; // Выдает идеальный нормированный сигнал
-    }
+    std::complex<double> transferFunction(double frequency) override { return {1.0, 0.0}; }
 };
 
-// Модель простого соединителя / волокна
 class FiberModel : public IComponentModel {
 public:
-    std::complex<double> transferFunction(double frequency) override {
-        return {0.9, 0.0}; // Пропускает сигнал, ослабляя его на 10% (коэффициент 0.9)
-    }
+    std::complex<double> transferFunction(double frequency) override { return {0.9, 0.0}; }
 };
