af751a1835562d9ae30bd0b118036c352311f3d6
HitryDmitry <ksenon.104@gmail.com>
2026-09-08 17:27:00 +0300

    Инициализация вектора нулями в конструкторе SPowerMatrix

--- domain/simulation/SMatrix.h
+++ domain/simulation/SMatrix.h
@@ -2,12 +2,15 @@
 
 #include <vector>
 
+// Класс, описывающий коэффициенты рассеяния по мощности
 class SPowerMatrix
 {
 public:
     SPowerMatrix(int dim = 1)
         : mDim(dim)
-    {}
+    {
+        mPowMatrix = std::vector<std::vector<double>>(dim, std::vector<double>(dim, 0.0));
+    }
 
     void setElement(int rowIdx, int colIdx, double val) { mPowMatrix.at(rowIdx).at(colIdx) = val; }
     double getElement(int rowIdx, int colIdx) const { return mPowMatrix.at(rowIdx).at(colIdx); }
