9ebc4c322a51ef733126c924f1f718a25cc249be
HitryDmitry <ksenon.104@gmail.com>
2026-09-09 16:44:47 +0300

    Тест на проверку корректности передаточной функции фотодетектора (уточнить значения)

--- tests/domain/SolverTest.cpp
+++ tests/domain/SolverTest.cpp
@@ -38,11 +38,49 @@ TEST_CASE("LinearSolver - Simple chain of components")
     auto simResult = linearSolver.solveFrequencyDomain(*graphPtr, circuitPtr, frequencies);
 
     // Значения на заданных частотах на входе фотодетектора (расчетные и эталонные)
-    std::vector<std::complex<double>> pdInputCalculated = simResult.frequencyResponses.at(pdInput);
-    std::vector<std::complex<double>> pdInputEtalonVals{{0, 0}, {1, 0}, {0, 0}};
+    std::vector<double> pdInputCalculated = simResult.frequencyResponses.at(pdInput);
+    std::vector<double> pdInputEtalonVals{0.0, 1.0, 0.0};
 
     // Значение мощности на входе фотодетектора
     for (size_t idx = 0; idx < pdInputEtalonVals.size(); idx++) {
         CHECK(pdInputCalculated[idx] == pdInputEtalonVals[idx]);
+    }
+}
+
+TEST_CASE("LinearSolver - Correct photodetector output")
+{
+    CircuitBuilder builder;
+    Solver linearSolver;
+
+    // Создаем схему: Laser -> Photodetector
+    auto laserId = builder.addComponent("laser");
+    auto pdId = builder.addComponent("photodetector");
+
+    builder.addWire(laserId, "out", pdId, "opt_in");
+
+    auto circuit = builder.build();
+    auto circuitPtr = circuit.get();
+
+    auto graph = GraphBuilder::build(circuitPtr);
+    auto graphPtr = graph.get();
+
+    // Формируем массив частот [194, 194.5, 195 ГГц] на которых будет рассчитан результат
+    std::vector<double> frequencies{193 * Units::GHz, 193.5 * Units::GHz, 194 * Units::GHz};
+
+    // Выход фотодетектора
+    PinRef pdInput{pdId, builder.getPinIndex(pdId, "elec_out")};
+    // Добавляем его в точки наблюдения
+    graphPtr->observationPoints.push_back(pdInput);
+
+    // Вычисляем результат на этих частотах на входе фотодетектора
+    auto simResult = linearSolver.solveFrequencyDomain(*graphPtr, circuitPtr, frequencies);
+
+    // Значения на заданных частотах на выходе фотодетектора (расчетные и эталонные)
+    std::vector<double> pdOutCalculated = simResult.frequencyResponses.at(pdInput);
+    std::vector<double> pdOutEtalonVals{0.0, 1.0, 0.0};
+
+    // Значение мощности на входе фотодетектора
+    for (size_t idx = 0; idx < pdOutEtalonVals.size(); idx++) {
+        CHECK(pdOutCalculated[idx] == pdOutEtalonVals[idx]);
     }
 }
